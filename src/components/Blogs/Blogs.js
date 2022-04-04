4a49fed1d0e0094a64b042ca6925dba914be98a0
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-04-04 21:10:25 +0600

    questions to answer in blogs has been added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
         <div>
-            this is blogs page
+            <div>
+                <h1>What is Context Api?</h1>
+                <p>The React Context API is a method for a React app to efficiently produce global variables that can be passed around. This is the alternative to "prop drilling" . Context is also touted as an easier, lighter approach to state management using Redux.it also works a consumer and a provider.The React Context API is a method for a React app to efficiently produce global variables that can be passed around. This is the alternative to "prop drilling" . Context is also touted as an easier, lighter approach to state management using Redux.it also works a consumer and a provider. </p>
+            </div>
+            <div>
+                <h1>What is Semantic tag?</h1>
+                <p>In one words, a semantic tag clearly describes its meaning to both the browser and developer. Also, we can say that it provide information about the contents ot those tags that outsail just how they look on a page .##Purpose of semantic tag:By adding semantic tags to your document, you provide additional information about that document, which aids in communication. Specifically, semantic tags make it clear to the browser what the meaning of a page and its content is. Some example of Semantic are given below: 1.form tag 2. table 3.and article.  </p>
+            </div>
         </div>
     );
 };
